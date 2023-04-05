d31db807e91dbd51a751a6d64e52aa4866dca57c
Karan Parashar <karan@pixelos.net>
2023-04-05 18:17:21 +0545

    scripts: Import fedora-workstation script
    * From [1]
    
    [1]: https://github.com/whyredfire/scripts
    
    Co-authored-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I85de6204afc4f4862afb010a751a729025b1e7e7

---
+++ fedora-workstation.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+#
+# Script to setup Fedora 37 Workstation
+#
+# Usage:
+#        ./fedora-workstation.sh
+#
+
+cd $HOME
+
+# Enable RPM Fusion
+echo -e "Enabling RPM Fusion\n"
+sudo dnf install -y \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Add flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install packages
+echo -e "Installing and updating dnf packages ...\n"
+sudo dnf install -y \
+    android-tools \
+    discord \
+    gnome-extensions-app.x86_64 \
+    gnome-tweaks \
+    htop \
+    imwheel \
+    neofetch \
+    nload \
+    pavucontrol \
+    telegram-desktop \
+    tldr
+
+# docker
+sudo dnf remove -y docker \
+                   docker-client \
+                   docker-client-latest \
+                   docker-common \
+                   docker-latest \
+                   docker-latest-logrotate \
+                   docker-logrotate \
+                   docker-selinux \
+                   docker-engine-selinux \
+                   docker-engine
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+
+# battop
+echo -e "\nInstalling battop..."
+wget https://github.com/svartalf/rust-battop/releases/download/v0.2.4/battop-v0.2.4-x86_64-unknown-linux-gnu -O battop
+sudo mv battop /usr/bin/
+sudo chmod +x /usr/bin/battop
+
+# gnome extensions
+function gnome_extensions(){
+array=(https://extensions.gnome.org/extension/779/clipboard-indicator/
+https://extensions.gnome.org/extension/19/user-themes/
+https://extensions.gnome.org/extension/1460/vitals/
+https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
+)
+
+for i in "${array[@]}"
+do
+    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+    fi
+    gnome-extensions enable ${EXTENSION_ID}
+    rm ${EXTENSION_ID}.zip
+done
+}
+
+# Multimedia plugins
+echo -e "\nInstalling multimedia plugins..."
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install -y lame\* --exclude=lame-devel
+sudo dnf group upgrade -y --with-optional Multimedia
+
+# vscode
+echo -e "\nInstalling Visual Studio Code..."
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf check-update
+sudo dnf install -y code
+
+# Caffeine
+echo -e "\nInstalling Caffeine extension..."
+git clone https://github.com/eonpatapon/gnome-shell-extension-caffeine
+cd gnome-shell-extension-caffeine
+make build
+make install
+cd ..
+rm -rf gnome-shell-extension-caffeine
+
+# git-cli
+echo -e "\nInstalling git-cli..."
+sudo dnf install 'dnf-command(config-manager)'
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install -y gh
+
+# Platform tools
+echo -e "\nInstalling Android SDK platform tools..."
+wget -q https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip -qq platform-tools-latest-linux.zip
+rm platform-tools-latest-linux.zip
+
+echo -e "\nSetting up android udev rules..."
+git clone https://github.com/M0Rf30/android-udev-rules.git
+cd android-udev-rules
+sudo cp -v 51-android.rules /etc/udev/rules.d/51-android.rules
+sudo chmod a+r /etc/udev/rules.d/51-android.rules
+sudo cp android-udev.conf /usr/lib/sysusers.d/
+sudo systemd-sysusers
+sudo gpasswd -a $(whoami) adbusers
+sudo udevadm control --reload-rules
+sudo systemctl restart systemd-udevd.service
+adb kill-server
+rm -rf android-udev-rules
+
+# Configure git
+echo -e "\nSetting up Git..."
+
+git config --global user.email "karan@pixelos.net"
+git config --global user.name "Karan Parashar"
+
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
+git config --global alias.f 'fetch'
+git config --global alias.m 'merge'
+git config --global alias.rb 'rebase'
+git config --global alias.rs 'reset'
+git config --global alias.ck 'checkout'
+git config --global credential.helper 'cache --timeout=99999999'
+git config --global core.editor "nano"
+
+# gnome shell presets
+echo -e "\nSetting gnome shell presets..."
+gsettings set org.gnome.shell disable-user-extensions false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.interface locate-pointer true
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+# Optimize boot time, it takes the longest time while booting
+sudo systemctl disable NetworkManager-wait-online.service
+
+echo -e "\nInstalling gnome-extensions..."
+gnome_extensions
+
+# oh-my-bash
+echo -e "\nSetup oh-my-bash..."
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+echo "#oh-my-bash" >> ~/.bashrc
+echo DISABLE_AUTO_UPDATE=true >> ~/.bashrc
