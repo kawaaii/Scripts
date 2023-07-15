aee7b30e640eda0ae3e6486b8a8c73481b3513b1
Ashwin Devi Srinivasan <ashwin@pixelos.net>
2023-07-15 08:56:26 +0400

    minor fixup

--- android/python/find_duplicates.py
+++ android/python/find_duplicates.py
@@ -40,4 +40,4 @@ if __name__ == "__main__":
   else:
     print("Duplicate entries:", end="")
     for duplicate in duplicates:
-      print("  " + duplicate.replace("\n", "")
+      print("  " + duplicate.replace("\n", ""))
