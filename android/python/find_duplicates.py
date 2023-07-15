98a8f17bca41585b2bab7be4ca6ea05c3184284b
hridaya <info.hridayaprajapati@gmail.com>
2023-07-15 07:21:06 +0545

    android: python: Add a script to find out the duplicate entries in proprietary-files list
    Change-Id: I61e619b06fe7ade7d6ee9d801aaf914240da8961

---
+++ android/python/find_duplicates.py
@@ -0,0 +1,41 @@
+import sys
+
+def find_duplicates(filename):
+  """Finds duplicate entries in a file.
+
+  Args:
+    filename: The name of the file to search.
+
+  Returns:
+    A list of duplicate entries.
+  """
+
+  with open(filename, "r") as f:
+    lines = f.readlines()
+
+  seen = set()
+  duplicates = []
+
+  for line in lines:
+    line = line.strip()
+    if line in seen:
+      duplicates.append(line)
+    else:
+      seen.add(line)
+
+  return duplicates
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: python find_duplicates.py <filename>")
+    sys.exit(1)
+
+  filename = sys.argv[1]
+  duplicates = find_duplicates(filename)
+
+  if not duplicates:
+    print("No duplicate entries found.")
+  else:
+    print("Duplicate entries:", end="")
+    for duplicate in duplicates:
+      print("  " + duplicate, end="")
