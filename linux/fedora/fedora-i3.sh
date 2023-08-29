86f0c2a215c121e691a68382bd3a28e4c48e2031
hridaya <info.hridayaprajapati@gmail.com>
2023-08-29 17:22:15 +0545

    linux: Add setup script for fedora-i3 spin
    Change-Id: I8e9bf6f7f351e5f1b443e68792ed28338a46239c
    Signed-off-by: hridaya <info.hridayaprajapati@gmail.com>

---
+++ linux/fedora/fedora-i3.sh
@@ -0,0 +1,31 @@
+# Speed up dnf
+echo "max_parallel_downloads=5" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+sudo dnf update --refresh
+
+# Enable RPM Fusion
+sudo dnf install -y \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Install packages
+sudo dnf update -y
+sudo dnf install -y \
+  dnf-plugins-core \
+  htop \
+  neofetch \
+  lxterminal \
+  fish \
+  flatpak \
+  telegram-desktop \
+  google-roboto-mono-fonts \
+  ripgrep \
+  git
+
+# Enable flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install -y code
